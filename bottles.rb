224df0263fa834e8cdc80ca126c8d413f09075ff
techproject <codingusage@gmail.com>
2015-08-08 21:10:28 -0700

    This is the bottles program.

---
+++ bottles.rb
@@ -0,0 +1,9 @@
+number = 99
+
+while number >= 1
+	puts """#{number} bottles of beer on the wall.\
+		#{number} bottles of beer.  You take one down,\
+		pass it around, #{number} bottles of beer on the wall."""
+	number -= 1	
+end
+
