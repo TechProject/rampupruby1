a76e96b1c3da4f1ad9a8bbba2322f6f819811974
techproject <codingusage@gmail.com>
2015-08-09 19:02:43 -0700

    just another update

---
+++ gma.rb
@@ -0,0 +1,16 @@
+class Gma_conversation
+
+	user_input = ""
+	gresponse1 = "HUH?!, SPEAK UP SONNY!"
+	gresponse2 = "NO, NOT SINCE #{number}"
+	number = rand(1930..1980)
+
+	print "What do you have to say to your Grandmother? "
+	user_input = gets.chomp
+
+	if user_input == user_input.downcase 
+		puts gresponse1
+	elsif user_input == user_input.upcase
+		puts gresponse2
+	end
+end
