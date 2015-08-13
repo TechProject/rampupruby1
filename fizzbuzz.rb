98bdf2b45eae4e9cc2aaeaa0c9354faf95a713d4
techproject <codingusage@gmail.com>
2015-08-13 19:33:07 -0400

    Another update

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,22 +6,19 @@
 
 # print fizzbuzz if # is divisable by both 5 and 3
 
-array = (1..100).to_a
+numbers = (1..100).to_a
 puts array
 
-array.each do |number|
+numbers.each do |number|
 puts number
 
 	if number % 3 == 0
 	puts "fizz"
 	end
-
-	if number % 5 == 0
+	elsif number % 5 == 0
 		puts "buzz"
 	end
-
-	if number % 3 == 0 or 5 == 0
+	else number % 3 == 0 or 5 == 0
 		puts "fizzbuzz"
 	end
-
 end
